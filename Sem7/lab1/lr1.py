70fe061baa4865e8dc84855dd6a0f5d6a0953342
Suslikov Daniil <danyvs@mail.ru>
2021-10-23 16:02:07 +0300

    lr1

--- Sem7/lab1/lr1.py
+++ Sem7/lab1/lr1.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
-from math import factorial, sqrt, exp
 from scipy.stats import poisson
 import numpy as np
-
 
 def uniform_distribution(a, b, x):
     if (x < a):
@@ -11,7 +9,6 @@ def uniform_distribution(a, b, x):
         return 1
     else:
         return (x - a) / (b - a)
-
 
 def uniform_density(a, b, x):
     if (a <= x <= b):
@@ -27,12 +24,11 @@ def poisson_distribution(x, mu):
 def poisson_density(x, mu):
     return poisson(mu).pmf(x)
 
-
-def draw_graphics(x, y_function, y_density, name):
+def draw_graphs(x, y_distribution, y_density, name):
     fig, axs = plt.subplots(2, figsize=(8, 6))
 
     fig.suptitle(name)
-    axs[0].plot(x, y_function, color='blue')
+    axs[0].plot(x, y_distribution, color='blue')
     axs[1].plot(x, y_density, color='green')
 
     axs[0].set_xlabel('x')
@@ -45,7 +41,6 @@ def draw_graphics(x, y_function, y_density, name):
     axs[1].grid(True)
     plt.show()
 
-
 def main():
     a = float(input("Input a: "))
     b = float(input("Input b: "))
@@ -53,17 +48,16 @@ def main():
     delta = b - a
     x = np.arange(a - delta / 2, b + delta / 2, 0.001)
     
-    y_function = [uniform_distribution(a, b, _x) for _x in x]
+    y_distribution = [uniform_distribution(a, b, _x) for _x in x]
     y_density = [uniform_density(a, b, _x) for _x in x]
-    draw_graphics(x, y_function, y_density, 'Равномерное распределение')
+    draw_graphs(x, y_distribution, y_density, 'Равномерное распределение')
 
     mu = float(input("Input mu: "))    
     x = np.arange(-1, 20)
-    y_function = poisson_distribution(x, mu)
+    y_distribution = poisson_distribution(x, mu)
     y_density = poisson_density(x, mu)
 
-    draw_graphics(x, y_function, y_density, 'Пуассоновское распределение')
-
+    draw_graphs(x, y_distribution, y_density, 'Пуассоновское распределение')
 
 if __name__ == '__main__':
     main()
